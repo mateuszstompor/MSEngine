07c12a647dc83441f8d7e4b04c16a1b4517f121d
mateusz <mateusz.stompor@gmail.com>
2017-09-02 11:33:41 +0200

    added texture wrapping.

--- sourceFiles/CoreEngine/Render/MSTextureOpenGL.m
+++ sourceFiles/CoreEngine/Render/MSTextureOpenGL.m
@@ -32,13 +32,12 @@
 -(void)loadToGraphicsMemory{
         glGenTextures(1, &self->_textureID);
         glBindTexture(GL_TEXTURE_2D, _textureID);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);    
-//            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-//            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
-        glBindTexture(GL_TEXTURE_2D, 0);
 }
 
 -(void) bindItself{
